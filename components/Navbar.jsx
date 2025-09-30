555d640c3cf3a64f113d958f26655d76679bb522
Coded-Adil <itsadil2022@gmail.com>
2025-10-01 00:17:31 +0600

    maked some components like navbar and footer

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold">Evently</h1>
+      <div className="space-x-4">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
+}
